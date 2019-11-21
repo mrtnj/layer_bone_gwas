c331dec5f9a13241935cc2016435e6aedab21ab4
martin <martin.johnsson@roslin.ed.ac.uk>
2019-11-21 16:38:04 +0100

    Turn marker annotation into a fasta file for alignment

---
+++ R/prepare_marker_fasta.R
@@ -0,0 +1,24 @@
+
+
+## Make a fasta file to move markers to Galgal6
+
+library(Biostrings)
+library(readr)
+
+
+
+annotation <- read_csv("RA-1698_181010_ResultReport/Chicken_50K_CobbCons_15000986_A.csv",
+                       skip = 7)
+
+
+seq <- annotation$SourceSeq[!is.na(annotation$SourceSeq)]
+
+seq <- sub(seq,
+           pattern = "\\[.*\\]",
+           replacement = "N")
+names(seq) <- annotation$Name[!is.na(annotation$SourceSeq)]
+
+fasta <- DNAStringSet(seq)
+
+writeXStringSet(fasta,
+                file = "alignment/source_seq.fasta")
