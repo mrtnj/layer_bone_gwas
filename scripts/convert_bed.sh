5edc9e03404c109549ec6c91a78874e6c9ad150e
martin <martin.johnsson@roslin.ed.ac.uk>
2019-11-25 14:20:33 +0100

    Run both pen and cage; add filtering to remove monomorphic SNPs

--- scripts/convert_bed.sh
+++ scripts/convert_bed.sh
@@ -7,6 +7,16 @@ set -eu
 ## Convert ped to bed with plink
 
 
-$PLINK_PATH/plink --file gwas/pen \
+$PLINK_PATH/plink --allow-extra-chr \
+		  --chr-set 40 \
+		  --file gwas/pen \
 		  --make-bed \
-		  --out gwas/pen
+		  --out gwas/pen \
+		  --maf 0.0001
+
+$PLINK_PATH/plink --allow-extra-chr \
+		  --chr-set 40 \
+		  --file gwas/cage \
+		  --make-bed \
+		  --out gwas/cage \
+		  --maf 0.0001
