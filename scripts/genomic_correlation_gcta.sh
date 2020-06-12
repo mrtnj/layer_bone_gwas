038b4629cdef9ec04c28abb0a8853450c4ccb14f
martin <martin.johnsson@roslin.ed.ac.uk>
2020-06-12 14:56:41 +0200

    Run weight too

--- scripts/genomic_correlation_gcta.sh
+++ scripts/genomic_correlation_gcta.sh
@@ -5,39 +5,74 @@ GCTA_PATH=~/tools/gcta/gcta_1.93.0beta_mac/bin/
 
 set -eu
 
-## Prepare files for GCTA 
+## Load
 
 $PLINK_PATH/plink --allow-extra-chr \
 		  --chr-set 40 \
 		  --file gwas/all \
 		  --make-bed \
-		  --out genomic_correlation/all \
+		  --out genomic_correlation/load_N/all \
 		  --maf 0.0001 \
 		  --chr 1-28
 
 $GCTA_PATH/gcta64 --bfile genomic_correlation/all \
 		  --make-grm-bin \
-		  --out genomic_correlation/gcta \
+		  --out genomic_correlation/load_N/gcta \
 		  --autosome-num 28 --autosome
 
 $GCTA_PATH/gcta64 --reml \
-		  --grm-bin genomic_correlation/gcta \
-		  --pheno genomic_correlation/pheno.txt \
-		  --qcovar genomic_correlation/qc.txt \
-		  --covar genomic_correlation/cc.txt \
-		  --out genomic_correlation/gcta_cage
+		  --grm-bin genomic_correlation/load_N/gcta \
+		  --pheno genomic_correlation/load_N/pheno.txt \
+		  --qcovar genomic_correlation/load_N/qc.txt \
+		  --covar genomic_correlation/load_N/cc.txt \
+		  --out genomic_correlation/load_N/gcta_cage
 
 $GCTA_PATH/gcta64 --reml \
-		  --grm-bin genomic_correlation/gcta \
-		  --pheno genomic_correlation/pheno.txt \
+		  --grm-bin genomic_correlation/load_N/gcta \
+		  --pheno genomic_correlation/load_N/pheno.txt \
 		  --mpheno 2 \
-		  --qcovar genomic_correlation/qc.txt \
-		  --covar genomic_correlation/cc.txt \
-		  --out genomic_correlation/gcta_pen
+		  --qcovar genomic_correlation/load_N/qc.txt \
+		  --covar genomic_correlation/load_N/cc.txt \
+		  --out genomic_correlation/load_N/gcta_pen
 
 $GCTA_PATH/gcta64 --reml-bivar \
-		  --grm-bin genomic_correlation/gcta \
-		  --pheno genomic_correlation/pheno.txt \
-		  --qcovar genomic_correlation/qc.txt \
-		  --covar genomic_correlation/cc.txt \
-		  --out genomic_correlation/gcta_bivar
+		  --grm-bin genomic_correlation/load_N/gcta \
+		  --pheno genomic_correlation/load_N/pheno.txt \
+		  --qcovar genomic_correlation/load_N/qc.txt \
+		  --covar genomic_correlation/load_N/cc.txt \
+		  --out genomic_correlation/load_N/gcta_bivar
+
+
+## Weight
+
+$PLINK_PATH/plink --allow-extra-chr \
+		  --chr-set 40 \
+		  --file gwas/all \
+		  --make-bed \
+		  --out genomic_correlation/weight/all \
+		  --maf 0.0001 \
+		  --chr 1-28
+
+$GCTA_PATH/gcta64 --bfile genomic_correlation/all \
+		  --make-grm-bin \
+		  --out genomic_correlation/weight/gcta \
+		  --autosome-num 28 --autosome
+
+$GCTA_PATH/gcta64 --reml \
+		  --grm-bin genomic_correlation/weight/gcta \
+		  --pheno genomic_correlation/weight/pheno.txt \
+		  --covar genomic_correlation/weight/cc.txt \
+		  --out genomic_correlation/weight/gcta_cage
+
+$GCTA_PATH/gcta64 --reml \
+		  --grm-bin genomic_correlation/weight/gcta \
+		  --pheno genomic_correlation/weight/pheno.txt \
+		  --mpheno 2 \
+		  --covar genomic_correlation/weight/cc.txt \
+		  --out genomic_correlation/weight/gcta_pen
+
+$GCTA_PATH/gcta64 --reml-bivar \
+		  --grm-bin genomic_correlation/weight/gcta \
+		  --pheno genomic_correlation/weight/pheno.txt \
+		  --covar genomic_correlation/weight/cc.txt \
+		  --out genomic_correlation/weight/gcta_bivar
