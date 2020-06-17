5f17c6b35b1657c4339fe75c714204e68532f35e
martin <martin.johnsson@roslin.ed.ac.uk>
2020-06-17 17:23:19 +0200

    Fix indentation

--- scripts/run_gwas.sh
+++ scripts/run_gwas.sh
@@ -7,30 +7,30 @@ function run_gwas () {
     COVAR_FILE=$4
 
     if [ ! -d gwas/$NAME ]; then
-	mkdir gwas/$NAME
+	     mkdir gwas/$NAME
     fi
 
     cd gwas/$NAME
 
     if [ ! -f $NAME.bed ]; then
-	ln -s ../$PLINK_BASE.bim $NAME.bim
-	ln -s ../$PLINK_BASE.bed $NAME.bed
-	ln -s ../$PLINK_BASE.map $NAME.map
-	ln -s ../$TRAIT_FILE $NAME.fam
-	if [ COVAR_FILE != "NULL" ]; then
-	    ln -s ../$COVAR_FILE covariates.txt
-	fi
+	    ln -s ../$PLINK_BASE.bim $NAME.bim
+	    ln -s ../$PLINK_BASE.bed $NAME.bed
+	    ln -s ../$PLINK_BASE.map $NAME.map
+	    ln -s ../$TRAIT_FILE $NAME.fam
+	    if [ COVAR_FILE != "NULL" ]; then
+	        ln -s ../$COVAR_FILE covariates.txt
+	    fi
     fi
 
     if [ $COVAR_FILE != "NULL" ]; then
-	$GEMMA_PATH/gemma -bfile $NAME \
+	    $GEMMA_PATH/gemma -bfile $NAME \
 			  -k ../output/${PLINK_BASE}_grm.sXX.txt \
 			  -c covariates.txt \
 			  -lmm 4 \
 			  -o $NAME
     fi
     if [ $COVAR_FILE == "NULL" ]; then
-	$GEMMA_PATH/gemma -bfile $NAME \
+	    $GEMMA_PATH/gemma -bfile $NAME \
 			  -k ../output/${PLINK_BASE}_grm.sXX.txt \
 			  -lmm 4 \
 			  -o $NAME
