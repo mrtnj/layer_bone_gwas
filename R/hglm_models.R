f78f9881b34715f055ecda04e25e31395c87ff93
martin <martin.johnsson@roslin.ed.ac.uk>
2020-08-07 17:18:36 +0200

    Add models that include all chickens

--- R/hglm_models.R
+++ R/hglm_models.R
@@ -18,48 +18,123 @@ pen_missing <- is.na(pen_load$X6)
 pen_load <- pen_load[!pen_missing,]
 
 
+all_load <- read_delim("gwas/all_load_N/all_load_N.fam", col_names = FALSE, delim = " ")
+
+all_missing <- is.na(all_load$X6)
+
+all_load <- all_load[!all_missing,]
+
+
 ## Load GRM from Gemma and decompose
 
 pen_GRM <- read_tsv("gwas/output/pen_grm.sXX.txt", col_names = FALSE)
 
-svd_pen <- svd(pen_GRM[!pen_missing, !pen_missing])
+all_GRM <- read_tsv("gwas/output/all_grm.sXX.txt", col_names = FALSE)
 
-Z_grm_pen <- svd_pen$u %*% diag(sqrt(svd_pen$d))
+decompose_grm <- function(grm, missing) {
+    svd <- svd(grm[!missing, !missing])
+    svd$u %*% diag(sqrt(svd$d))
+}
+
+Z_grm_pen <- decompose_grm(pen_GRM,
+                           pen_missing)
+
+Z_grm_all <- decompose_grm(all_GRM,
+                           all_missing)
 
 
 ## Get group data
 
 pheno <- readRDS("outputs/pheno.Rds")
 
-pen_covar <- filter(pheno,
-                    animal_id %in% pen_load$X1 &
-                    !is.na(load_N))[, c("animal_id", "group", "weight")]
+get_covar <- function(pheno,
+                      load_data) {
 
-pen_covar <- pen_covar[order(pen_covar$animal_id),]
+    covar <- filter(pheno,
+                        animal_id %in% load_data$X1 &
+                            !is.na(load_N))[, c("animal_id", "group", "weight")]
+    
+    covar <- covar[order(covar$animal_id),]
+    
+    assert_that(identical(covar$animal_id, load_data$X1))
 
-assert_that(identical(pen_covar$animal_id, pen_load$X1))
+    covar
+}
+
+pen_covar <- get_covar(pheno,
+                       pen_load)
+
+all_covar <- get_covar(pheno,
+                       all_load)
+
+all_covar$group_cages_combined <- ifelse(all_covar$group > 2000,
+                                         2000,
+                                         all_covar$group)
 
 Z_pen_group <- model.matrix(~ factor(group), pen_covar)
 
+Z_all_group <- model.matrix(~ factor(group), all_covar)
 
-## Model
-
-model <- hglm(y = pen_load$X6,
-              X = model.matrix(~ 1 + weight, pen_covar),
-              Z = cbind(Z_grm_pen, Z_pen_group),
-              RandC = c(ncol(Z_grm_pen), ncol(Z_pen_group)))
+Z_all_group_cages_combined <- model.matrix(~ factor(group_cages_combined), all_covar)
 
 
-h2 <- model$varRanef[1] / (model$varFix + sum(model$varRanef))
-ratio_group <- model$varRanef[2] / (model$varFix + sum(model$varRanef))
+get_h2 <- function(model) {
+    model$varRanef[1] / (model$varFix + sum(model$varRanef))
+}
+
+get_group_ratio <- function(model) {
+    model$varRanef[2] / (model$varFix + sum(model$varRanef))
+}
+
+## Model of pen animals with random pen group
+
+model_pen_random <- hglm(y = pen_load$X6,
+                         X = model.matrix(~ 1 + weight, pen_covar),
+                         Z = cbind(Z_grm_pen, Z_pen_group),
+                         RandC = c(ncol(Z_grm_pen), ncol(Z_pen_group)))
 
 
-## Model without pen
-
-model_without <- hglm(y = pen_load$X6,
-                      X = model.matrix(~ 1 + weight, pen_covar),
-                      Z = Z_grm_pen)
-
-h2_without <- model_without$varRanef[1] / (model_without$varFix + sum(model_without$varRanef))
+h2_pen_random <- get_h2(model_pen_random)
+ratio_group <- get_group_ratio(model_pen_random)
 
 
+## Model of pen animals without random pen group
+
+model_pen_without <- hglm(y = pen_load$X6,
+                          X = model.matrix(~ 1 + weight, pen_covar),
+                          Z = Z_grm_pen)
+
+h2_pen_without <- get_h2(model_pen_without)
+
+
+## All animals with random pen/cage group
+
+model_all_random <- hglm(y = all_load$X6,
+                         X = model.matrix(~ 1 + weight, all_covar),
+                         Z = cbind(Z_grm_all, Z_all_group),
+                         RandC = c(ncol(Z_grm_all), ncol(Z_all_group)))
+
+
+h2_all_random <- get_h2(model_all_random)
+ratio_group_all_random <- get_group_ratio(model_all_random)
+
+
+## All animals with random pen group, cages combined into one group;
+
+model_all_random_cages_combined <- hglm(y = all_load$X6,
+                                        X = model.matrix(~ 1 + weight, all_covar),
+                                        Z = cbind(Z_grm_all, Z_all_group_cages_combined),
+                                        RandC = c(ncol(Z_grm_all), ncol(Z_all_group_cages_combined)))
+
+
+h2_all_random_cages_combined <- get_h2(model_all_random_cages_combined)
+ratio_group_all_random_cages_combined <- get_group_ratio(model_all_random_cages_combined)
+
+
+## All animals without random pen/cage group
+
+model_all_without <- hglm(y = all_load$X6,
+                          X = model.matrix(~ 1 + weight, all_covar),
+                          Z = Z_grm_all)
+
+h2_all_without <- get_h2(model_all_without)
