ef370a933aac07f7114a34ab4c073b71b00800f8
Martin Johnsson <sorill@gmail.com>
2022-07-19 14:12:23 +0200

    Overalap associations and find regions for cross-separated scans

---
+++ R/hglm_gwas_overlap_crosses_separate.R
@@ -0,0 +1,325 @@
+
+## Find regions around GWAS hits for summary table
+
+##library(biomaRt)
+library(dplyr)
+library(GenomicRanges)
+library(readr)
+library(stringr)
+library(purrr)
+
+## Bone strength
+
+gwas_pen_bovans_load <- readRDS("outputs/gwas_pen_bovans_load_coord.Rds")
+gwas_pen_bovans_load$pretty_name <- "bone strength"
+gwas_pen_bovans_load$scan <- "PEN"
+gwas_pen_bovans_load$cross <- "Bovans"
+
+gwas_pen_lsl_load <- readRDS("outputs/gwas_pen_lsl_load_coord.Rds")
+gwas_pen_lsl_load$pretty_name <- "bone strength"
+gwas_pen_lsl_load$scan <- "PEN"
+gwas_pen_lsl_load$cross <- "LSL"
+
+gwas_cage_bovans_load <- readRDS("outputs/gwas_cage_bovans_load_coord.Rds")
+gwas_cage_bovans_load$pretty_name <- "bone strength"
+gwas_cage_bovans_load$scan <- "CAGE"
+gwas_cage_bovans_load$cross <- "Bovans"
+
+gwas_cage_lsl_load <- readRDS("outputs/gwas_cage_lsl_load_coord.Rds")
+gwas_cage_lsl_load$pretty_name <- "bone strength"
+gwas_cage_lsl_load$scan <- "CAGE"
+gwas_cage_lsl_load$cross <- "LSL"
+
+gwas_all_bovans_load <- readRDS("outputs/gwas_all_bovans_load_coord.Rds")
+gwas_all_bovans_load$pretty_name <- "bone strength"
+gwas_all_bovans_load$scan <- "JOINT"
+gwas_all_bovans_load$cross <- "Bovans"
+
+gwas_all_lsl_load <- readRDS("outputs/gwas_all_lsl_load_coord.Rds")
+gwas_all_lsl_load$pretty_name <- "bone strength"
+gwas_all_lsl_load$scan <- "JOINT"
+gwas_all_lsl_load$cross <- "LSL"
+
+
+
+## Body weight
+
+gwas_pen_bovans_weight <- readRDS("outputs/gwas_pen_bovans_weight_coord.Rds")
+gwas_pen_bovans_weight$pretty_name <- "body weight"
+gwas_pen_bovans_weight$scan <- "PEN"
+gwas_pen_bovans_weight$cross <- "Bovans"
+
+gwas_pen_lsl_weight <- readRDS("outputs/gwas_pen_lsl_weight_coord.Rds")
+gwas_pen_lsl_weight$pretty_name <- "body weight"
+gwas_pen_lsl_weight$scan <- "PEN"
+gwas_pen_lsl_weight$cross <- "LSL"
+
+gwas_cage_bovans_weight <- readRDS("outputs/gwas_cage_bovans_weight_coord.Rds")
+gwas_cage_bovans_weight$pretty_name <- "body weight"
+gwas_cage_bovans_weight$scan <- "CAGE"
+gwas_cage_bovans_weight$cross <- "Bovans"
+
+gwas_cage_lsl_weight <- readRDS("outputs/gwas_cage_lsl_weight_coord.Rds")
+gwas_cage_lsl_weight$pretty_name <- "body weight"
+gwas_cage_lsl_weight$scan <- "CAGE"
+gwas_cage_lsl_weight$cross <- "LSL"
+
+gwas_all_bovans_weight <- readRDS("outputs/gwas_all_bovans_weight_coord.Rds")
+gwas_all_bovans_weight$pretty_name <- "body weight"
+gwas_all_bovans_weight$scan <- "JOINT"
+gwas_all_bovans_weight$cross <- "Bovans"
+
+gwas_all_lsl_weight <- readRDS("outputs/gwas_all_lsl_weight_coord.Rds")
+gwas_all_lsl_weight$pretty_name <- "body weight"
+gwas_all_lsl_weight$scan <- "JOINT"
+gwas_all_lsl_weight$cross <- "LSL"
+
+
+## pQCT & TGA
+
+gwas_pen_bovans <- readRDS("outputs/gwas_pen_bovans_bone_phenotypes_coord.Rds")
+gwas_cage_bovans <- readRDS("outputs/gwas_cage_bovans_bone_phenotypes_coord.Rds")
+gwas_all_bovans <- readRDS("outputs/gwas_all_bovans_bone_phenotypes_coord.Rds")
+
+gwas_pen_lsl <- readRDS("outputs/gwas_pen_lsl_bone_phenotypes_coord.Rds")
+gwas_cage_lsl <- readRDS("outputs/gwas_cage_lsl_bone_phenotypes_coord.Rds")
+gwas_all_lsl <- readRDS("outputs/gwas_all_lsl_bone_phenotypes_coord.Rds")
+
+
+gwas_pen_bovans$scan <- "PEN"
+gwas_pen_bovans$cross <- "Bovans"
+
+gwas_pen_lsl$scan <- "PEN"
+gwas_pen_lsl$cross <- "LSL"
+
+gwas_cage_bovans$scan <- "CAGE"
+gwas_cage_bovans$cross <- "Bovans"
+
+gwas_cage_lsl$scan <- "CAGE"
+gwas_cage_lsl$cross <- "LSL"
+
+gwas_all_bovans$scan <- "JOINT"
+gwas_all_bovans$cross <- "Bovans"
+
+gwas_all_lsl$scan <- "JOINT"
+gwas_all_lsl$cross <- "LSL"
+
+
+cols <- c("pretty_name", "chr", "ps", "marker_id", "estimates", "LRT", "p", "scan", "cross")
+
+combined <- rbind(gwas_pen_bovans_load[, cols],
+                  gwas_pen_lsl_load[, cols],
+                  gwas_cage_bovans_load[, cols],
+                  gwas_cage_lsl_load[, cols],
+                  gwas_all_bovans_load[, cols],
+                  gwas_all_lsl_load[, cols],
+                  gwas_pen_bovans_weight[, cols],
+                  gwas_cage_bovans_weight[, cols],
+                  gwas_pen_lsl_weight[, cols],
+                  gwas_cage_lsl_weight[, cols],
+                  gwas_all_bovans_weight[, cols],
+                  gwas_all_lsl_weight[, cols],
+                  gwas_pen_bovans[, cols],
+                  gwas_pen_lsl[, cols],
+                  gwas_cage_bovans[, cols],
+                  gwas_cage_lsl[, cols],
+                  gwas_all_bovans[, cols],
+                  gwas_all_lsl[, cols])
+
+
+
+combined$pretty_name <- paste(combined$pretty_name,
+                              " (",
+                              combined$cross,
+                              ", ",
+                              combined$scan,
+                              ")",
+                              sep = "")
+
+
+write.csv(combined,
+          "tables/supplementary_data_gwas_summary_stats_crosses_separate.csv",
+          quote = FALSE,
+          row.names = FALSE)
+
+
+significant <- filter(combined, p < 5e-8)
+
+suggestive <- filter(combined, p < 1e-5)
+
+reduce_region <- function(trait_markers) {
+ 
+    marker_ranges <- GRanges(seqnames = trait_markers$chr,
+                             ranges = IRanges(trait_markers$ps - 5e6,
+                                              trait_markers$ps + 5e6))
+    GenomicRanges::reduce(marker_ranges)
+}
+
+
+hit_traits <- unique(c(significant$pretty_name, suggestive$pretty_name))
+
+significant_split <- lapply(hit_traits, function(trait) significant[significant$pretty_name == trait,])
+suggestive_split <- lapply(hit_traits, function(trait) suggestive[suggestive$pretty_name == trait,])
+
+names(significant_split) <- hit_traits
+names(suggestive_split) <- hit_traits
+
+significant_regions <- lapply(significant_split, reduce_region)
+suggestive_regions <- lapply(suggestive_split, reduce_region)
+
+suggestive_regions_pruned <- mapply(function(ranges1, ranges2) ranges1[!ranges1 %over% ranges2],
+                                    suggestive_regions,
+                                    significant_regions,
+                                    SIMPLIFY = FALSE)
+
+## We now have the regions that are significant/suggestive for each trait. Now
+## we need to go back and find the underlying SNPs
+
+
+## Function to extract overlapping SNPs from a GenomicRanges object of QTL regions from a scan
+
+get_snps <- function(regions, scan_name, combined) {
+    
+    snps_for_this_trait <- combined[combined$pretty_name == scan_name,]
+    
+    snp_ranges <- GRanges(seqnames = snps_for_this_trait$chr,
+                          ranges = IRanges(snps_for_this_trait$ps, snps_for_this_trait$ps),
+                          mcols = snps_for_this_trait)
+    
+    n_qtl <- length(regions)
+    
+    qtl_ranges_list <- vector(mode = "list",
+                              length = n_qtl)
+    
+    if (n_qtl > 0) {
+        
+        for (qtl_ix in 1:n_qtl) {
+            qtl_ranges_list[[qtl_ix]] <- subsetByOverlaps(snp_ranges,
+                                                          regions[qtl_ix])
+            
+        }
+    }
+    
+    qtl_ranges_list
+}
+
+
+## Find the most significant SNP in each region, extract statistics
+
+get_top_snps <- function(regions) {
+
+    n_qtl <- length(regions)
+    
+    top_snps <- vector(mode = "list",
+                       length = n_qtl)
+    
+    if (n_qtl > 0) {
+        
+        for (qtl_ix in 1:n_qtl) {
+            region_df <- as.data.frame(regions[[qtl_ix]])
+            top_snps[[qtl_ix]] <- region_df[region_df$mcols.p == min(region_df$mcols.p),]
+            
+            ## If there are more than one, pick the first
+            if (nrow(top_snps[[qtl_ix]]) > 1) {
+                top_snps[[qtl_ix]] <- top_snps[[qtl_ix]][1,]
+            }
+        }
+    }
+    
+    top_snps
+}
+
+
+## Apply this function to all the significant/suggestive scans
+
+snps_significant_regions <- vector(mode = "list",
+                                   length = length(significant_regions))
+
+top_snps_significant_regions <- vector(mode = "list",
+                                       length = length(significant_regions))
+
+for (scan_ix in 1:length(significant_regions)) {
+    snps_significant_regions[[scan_ix]] <- get_snps(significant_regions[[scan_ix]],
+                                                    names(significant_regions)[scan_ix],
+                                                    combined)
+    top_snps_significant_regions[[scan_ix]] <- get_top_snps(snps_significant_regions[[scan_ix]])
+}
+
+
+snps_suggestive_regions <- vector(mode = "list",
+                                  length = length(suggestive_regions_pruned))
+
+top_snps_suggestive_regions <- vector(mode = "list",
+                                      length = length(suggestive_regions_pruned))
+
+for (scan_ix in 1:length(suggestive_regions_pruned)) {
+    snps_suggestive_regions[[scan_ix]] <- get_snps(suggestive_regions_pruned[[scan_ix]],
+                                                   names(suggestive_regions_pruned)[scan_ix],
+                                                   combined)
+    top_snps_suggestive_regions[[scan_ix]] <- get_top_snps(snps_suggestive_regions[[scan_ix]])
+}
+
+
+
+
+## Make tables with top SNPs
+
+make_table <- function(scans_list,
+                       top_snps_list) {
+
+    significant_regions_df <- vector(mode = "list",
+                                     length = length(scans_list))
+    
+    for (scan_ix in 1:length(scans_list)) {
+        
+        regions <- as.data.frame(scans_list[[scan_ix]])
+        regions$seqnames <- as.character(regions$seqnames)
+        
+        n_qtl <- nrow(regions)
+        
+        if (n_qtl > 0) {
+            regions$trait <- names(scans_list)[scan_ix]
+            top_snps <- top_snps_list[[scan_ix]]
+            
+            regions$lead_p <- 0
+            regions$lead_pos <- 0
+            
+            for (region_ix in 1:n_qtl) {
+                regions$lead_p[region_ix] <- top_snps[[region_ix]]$mcols.p
+                regions$lead_pos[region_ix] <- top_snps[[region_ix]]$mcols.ps
+            }
+        }
+        
+        significant_regions_df[[scan_ix]] <- regions
+    }
+ 
+    Reduce(rbind, significant_regions_df)
+}
+
+significant_table <- make_table(significant_regions, top_snps_significant_regions)
+
+suggestive_table <- make_table(suggestive_regions_pruned, top_snps_suggestive_regions)
+
+
+
+significant_table <- significant_table[order(as.numeric(significant_table$seqnames),
+                                             significant_table$lead_pos),]
+
+suggestive_table <- suggestive_table[order(as.numeric(suggestive_table$seqnames),
+                                     suggestive_table$lead_pos),]
+
+
+
+
+cols <- c("trait", "seqnames", "lead_pos", "lead_p")
+
+write.csv(significant_table[, cols],
+          file = "tables/significant_gwas_table_crosses_separate.csv",
+          quote = TRUE,
+          row.names = FALSE)
+
+write.csv(suggestive_table[, cols],
+          file = "tables/suggestive_gwas_table_crosses_separate.csv",
+          quote = TRUE,
+          row.names = FALSE)
+
